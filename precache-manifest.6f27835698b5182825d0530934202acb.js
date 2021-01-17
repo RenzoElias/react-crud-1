69b4dff3d0df4cfd08f8965ac57ad0a192e5b3d5
RenzoElias <57267514+RenzoElias@users.noreply.github.com>
2021-01-17 02:14:55 -0500

    Updates

---
+++ precache-manifest.6f27835698b5182825d0530934202acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb966183146822e07f1f8ed314763a12",
+    "url": "/react-crud-1/index.html"
+  },
+  {
+    "revision": "56b2a9c06acfa48286dd",
+    "url": "/react-crud-1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2fe3b709ed43ba427002",
+    "url": "/react-crud-1/static/js/2.4f7fc74d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-crud-1/static/js/2.4f7fc74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b2a9c06acfa48286dd",
+    "url": "/react-crud-1/static/js/main.6cd762a2.chunk.js"
+  },
+  {
+    "revision": "c52d1d6a782387ffda15",
+    "url": "/react-crud-1/static/js/runtime-main.0aa3010a.js"
+  }
+]);
